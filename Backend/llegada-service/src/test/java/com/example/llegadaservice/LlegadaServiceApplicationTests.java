750c288e0fa0da0574c51e81e30003106e2518f0
cristowo <cristobal.marchant@usach.cl>
2023-05-25 21:16:28 -0400

    Inicialización de spring por cada clase

---
+++ Backend/llegada-service/src/test/java/com/example/llegadaservice/LlegadaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llegadaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlegadaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Backend/resultado-service/src/test/java/com/example/resultadoservice/ResultadoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resultadoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultadoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
