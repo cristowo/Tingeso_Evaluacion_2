8e1cf0a3c43314cd8c79ecc2e252c80fa375ce77
cristowo <cristobal.marchant@usach.cl>
2023-06-03 17:34:19 -0400

    modificacion a bootstrap

--- Backend/resultado-service/src/main/java/milkstgo/resultadoservice/ResultadoServiceApplication.java
+++ Backend/resultado-service/src/main/java/milkstgo/resultadoservice/ResultadoServiceApplication.java
@@ -2,8 +2,10 @@ package milkstgo.resultadoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ResultadoServiceApplication {
 
 	public static void main(String[] args) {
