9cc001bd78ebcab138f57474bef34d6e4d291a89
cristowo <cristobal.marchant@usach.cl>
2023-06-13 00:28:52 -0400

    Prueba

--- Backend/proveedor-service/src/main/java/milkstgo/proveedorservice/controllers/ProveedorController.java
+++ Backend/proveedor-service/src/main/java/milkstgo/proveedorservice/controllers/ProveedorController.java
@@ -4,7 +4,6 @@ import milkstgo.proveedorservice.entities.ProveedorEntity;
 import milkstgo.proveedorservice.services.ProveedorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
